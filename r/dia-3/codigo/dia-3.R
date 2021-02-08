f7898f2e3d8009017ff1bff3b61ad8ccdc770496
estebanrucan <errucan@uc.cl>
2021-02-07 23:12:57 -0300

    Día 3 en R terminado

---
+++ r/dia-3/codigo/dia-3.R
@@ -0,0 +1,18 @@
+require(tidyverse); require(plotly)
+
+data <- rio::import(here::here("codigo", "datos-sin-procesar", "owid-covid-data.csv"))
+
+chile_arg_peru_01_2021 <- data %>% 
+    select(location, date, new_cases) %>% 
+    filter(location %in% c("Chile", "Argentina", "Peru")) %>% 
+    filter(date >= "2021-01-01" & date <= "2021-01-31") %>% 
+    filter(!is.na(new_cases))
+
+ggplot(data = chile_arg_peru_01_2021) +
+    aes(x = date, y = new_cases, color = location) +
+    geom_line(size = 1) +
+    labs(x = "Fecha", y = "Casos Nuevos", title = "Nuevos casos por día en enero 2021",
+         subtitle = "Chile, Argentina y Perú") +
+    theme_bw()
+
+    
